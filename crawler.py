7aadf3b115cf5cb8a795f356b95237265185189c
Xin Yu Liu <xiliu@zuora.com>
2021-08-24 23:02:02 +0800

    retries 3x when connection err and folders are organized in tenderee name

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ import re
 import datetime
 import os
 import smtplib, ssl
+import time
 
 public_notice_root_page = 'http://ggzy.gz.gov.cn/jyywjsgcfwjzzbgg/index.jhtml'
 
@@ -30,9 +31,9 @@ def send_email(subject, msg):
         server.sendmail(sender_email, receiver_email, message)
 
 
-def crawl_single_page(root_page, working_dir, today, page_cnt):
+def crawl_single_page(root_page, output_dir, today, page_cnt):
     web_link_prefix = r'http://ggzy.gz.gov.cn/jyywjsgcfwjzzbgg/\d{6}.jhtml'#http://ggzy.gz.gov.cn/jyywjsgcfwjzzbgg/*.jhtml'
-    label_prefix = r'JG\d{4}\-\d{5}'
+    label_prefix = r'JG(.*?)]'
     date_prefix = r'<td><span>\d{4}\-\d{2}\-\d{2}</span></td>'
 
     page_idx = ''
@@ -52,36 +53,74 @@ def crawl_single_page(root_page, working_dir, today, page_cnt):
         d = re.search(r'\d{4}\-\d{2}\-\d{2}', raw_dates[i]).group()
         if d == today:
             url_info.append((web_links[i], labels[i], d))
-    
+    r.close()
     return url_info
 
+def get_tenderee(file_content):
+    tenderee = ""
+    tenderee_regexs = [r"标 单 位(.*?)</span></span>", r"招标单位：(.*?)&emsp;&emsp;</span>",r"招标人：(.*?)</span></li>"]
+    txt = []
+    for tr in tenderee_regexs:
+        txt = re.findall(tr, file_content)
+        if len(txt) == 1:
+            break
+    if len(txt) == 0:
+        return None
+    idx = txt[0].rfind('>')
+    tenderee_name = txt[0][idx+1:]
+    return tenderee_name
 
 
 cwd = os.getcwd()
-working_dir = os.path.join(cwd, '')
+output_dir = os.path.join(cwd, 'output')
 public_notice_items = []
-if not os.path.exists(working_dir):
+if not os.path.exists(output_dir):
     print("Working direcotry does not exist")
     exit()
 
 today = str(datetime.date.today())
 page = 1
 while True:
-    tmp = crawl_single_page(public_notice_root_page, working_dir, today, page)
+    tmp = crawl_single_page(public_notice_root_page, output_dir, today, page)
+    time.sleep(1)
     if len(tmp) == 0:
         break
     public_notice_items += tmp
     page += 1
-if len(public_notice_items) < 0:
-    exit()
 
-today_dir = os.path.join(working_dir, today)
 if len(public_notice_items) > 0:
-    if not os.path.exists(today_dir):
-        os.mkdir(today_dir)
     for notice in public_notice_items:
-        req = requests.get(notice[0], allow_redirects=True)
-        file_abs_path = os.path.join(today_dir, f"{notice[1]}.jhtml")
-        open(file_abs_path, 'wb').write(req.content)
-    send_email(f"There are {len(public_notice_items)} new public notices", "this is a test message from Jackie")
+        success = False
+        retry_cnt = 0
+        # if we got connection error, we will retry 3 times. 
+        while not success and retry_cnt < 3:
+            try:
+                success = True
+                req = requests.get(notice[0], allow_redirects=True)
+                tenderee_name = get_tenderee(req.text)
+                if tenderee_name == None:
+                    continue
+                tenderee_name = tenderee_name.replace('、', '-')
+                print(tenderee_name)
+
+                tenderee_dir = os.path.join(output_dir, tenderee_name)
+                if not os.path.exists(tenderee_dir):
+                    os.mkdir(tenderee_dir)
+
+                today_dir = os.path.join(tenderee_dir, today)
+                if not os.path.exists(today_dir):
+                    os.mkdir(today_dir)
+                file_abs_path = os.path.join(today_dir, f"{notice[1]}.jhtml")
+                open(file_abs_path, 'wb').write(req.content)
+
+                req.close()
+            except requests.exceptions.ConnectionError as e:
+                print(f"Got connection error {e} for {notice[0]} on {notice[2]}, will retry")
+                time.sleep(1)
+                retry_cnt += 1
+                success = False
+
+
+        
+    # send_email(f"There are {len(public_notice_items)} new public notices", "this is a test message from Jackie")
 
